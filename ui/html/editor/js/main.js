2a1a802bb797f72e2da2e3255e4b6b82940a76f9
laaposto <laaposto@gmail.com>
2017-05-24 18:05:25 +0300

    Added include method to editor

--- ui/html/editor/js/main.js
+++ ui/html/editor/js/main.js
@@ -32,7 +32,7 @@ $('#search_but').click(function () {
                     },
                     data: data,
                     success: function (json) {
-                        var subtitle = "";
+                        var subtitle = "", img_icon = "";
                         $('#ruleMatches').text(json.annotations.ruleMatches);
                         $('#topicOnlyMatches').text(json.annotations.topicOnlyMatches);
                         $('#topicMatches').text(json.annotations.topicMatches);
@@ -45,12 +45,25 @@ $('#search_but').click(function () {
                         if (json.entries.length > 0) {
                             $('#wmd-input').attr('contenteditable', 'false');
                             $('#syntax_but,#search_but,#save_but,#delete_but,#corpus_select').attr('disabled', 'disabled');
+                            switch ($('.active_match').attr('id')) {
+                                case "topicMatches":
+                                case "topicOnlyMatches":
+                                    img_icon = '<img class="exclude_topic" src="imgs/delete.png">';
+                                    break;
+                                case "ruleMatches":
+                                case "ruleOnlyMatches":
+                                    img_icon = '<img class="include_topic" src="imgs/add-icon.png">';
+                                    break;
+                                case "bothMatches":
+                                    img_icon = '';
+                                    break;
+                            }
                             for (var i = 0; i < json.entries.length; i++) {
                                 subtitle = "";
                                 if (json.entries[i].hasOwnProperty('subtitle')) {
                                     subtitle = '<h3 class="title_article subtitle"> <span>Subtitle:</span> ' + json.entries[i].subtitle + '</h3>';
                                 }
-                                $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '<img class="exclude_topic"src="imgs/delete.png"> </p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
+                                $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + img_icon + '</p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
                             }
                             var $articles_pagination = $('#articles_pagination');
                             $('#result_articles,#well_articles').show();
@@ -787,7 +800,7 @@ function parse_articles(page) {
         },
         data: data,
         success: function (json) {
-            var subtitle = "";
+            var subtitle = "", img_icon = "";
             $('#ruleMatches').text(json.annotations.ruleMatches);
             $('#topicOnlyMatches').text(json.annotations.topicOnlyMatches);
             $('#topicMatches').text(json.annotations.topicMatches);
@@ -796,12 +809,25 @@ function parse_articles(page) {
             $('#recall').text(json.annotations.recall);
             $('#precision').text(json.annotations.precision);
             $('#accuracy').text(json.annotations.accuracy);
+            switch ($('.active_match').attr('id')) {
+                case "topicMatches":
+                case "topicOnlyMatches":
+                    img_icon = '<img class="exclude_topic" src="imgs/delete.png">';
+                    break;
+                case "ruleMatches":
+                case "ruleOnlyMatches":
+                    img_icon = '<img class="include_topic" src="imgs/add-icon.png">';
+                    break;
+                case "bothMatches":
+                    img_icon = '';
+                    break;
+            }
             for (var i = 0; i < json.entries.length; i++) {
                 subtitle = "";
                 if (json.entries[i].hasOwnProperty('subtitle')) {
                     subtitle = '<h3 class="title_article subtitle"> <span>Subtitle:</span> ' + json.entries[i].subtitle + '</h3>';
                 }
-                $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '<img class="exclude_topic"src="imgs/delete.png"></p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
+                $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + img_icon + '</p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
             }
         },
         error: function () {
@@ -874,6 +900,18 @@ $("#result_articles").on("click", ".exclude_topic", function (e) {
         dataType: "json",
         success: function () {
             $this.parents('.article').remove();
+        },
+        async: true
+    });
+});
+$("#result_articles").on("click", ".include_topic", function (e) {
+    e.stopPropagation();
+    var $this = $(this);
+    $.ajax({
+        type: "PUT",
+        dataType: "json",
+        url: "http://" + window.location.hostname + ":5000/api/topics/" + $('#rule_topic').attr('data-id') + "?corpus=" + $('#corpus_select').val() + "&document_id=" + $this.parents('.article').attr('data-id'),
+        success: function () {
         },
         async: true
     });
@@ -1196,7 +1234,7 @@ $('#stats_articles td').click(function () {
             },
             data: data,
             success: function (json) {
-                var subtitle = "";
+                var subtitle = "", img_icon = "";
                 $('#result_articles').empty();
                 $('#ruleMatches').text(json.annotations.ruleMatches);
                 $('#topicOnlyMatches').text(json.annotations.topicOnlyMatches);
@@ -1206,13 +1244,26 @@ $('#stats_articles td').click(function () {
                 $('#recall').text(json.annotations.recall);
                 $('#precision').text(json.annotations.precision);
                 $('#accuracy').text(json.annotations.accuracy);
+                switch ($('.active_match').attr('id')) {
+                    case "topicMatches":
+                    case "topicOnlyMatches":
+                        img_icon = '<img class="exclude_topic" src="imgs/delete.png">';
+                        break;
+                    case "ruleMatches":
+                    case "ruleOnlyMatches":
+                        img_icon = '<img class="include_topic" src="imgs/add-icon.png">';
+                        break;
+                    case "bothMatches":
+                        img_icon = '';
+                        break;
+                }
                 if (json.entries.length > 0) {
                     for (var i = 0; i < json.entries.length; i++) {
                         subtitle = "";
                         if (json.entries[i].hasOwnProperty('subtitle')) {
                             subtitle = '<h3 class="title_article subtitle"> <span>Subtitle:</span> ' + json.entries[i].subtitle + '</h3>';
                         }
-                        $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '<img class="exclude_topic"src="imgs/delete.png"></p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
+                        $('#result_articles').append('<div class="article" data-id="' + json.entries[i].id + '"><div style="display: none" class="hidden_article"><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + '</p>' + subtitle + '<h3 class="title_body">Body:</h3>' + json.entries[i].body + '</div><p class="title_article"> <span>Title:</span> ' + json.entries[i].title + img_icon + '</p>' + subtitle + '<h3 class="title_body">Body:</h3><p class="desc_article">' + json.entries[i].body_paragraphs[0].paragraph + '</p></div>');
                     }
                     var $articles_pagination = $('#articles_pagination');
                     $('#stats_articles,#result_articles,#well_articles,#back_rules').show();
